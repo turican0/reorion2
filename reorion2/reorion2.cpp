41697866c5a186a3e35e4c1d2c2eaf161b13c008
Tomaš Veselý <turican0@gmail.com>
2026-07-14 15:16:22 +0200

    update

--- reorion2/reorion2.cpp
+++ reorion2/reorion2.cpp
@@ -10,3 +10,11 @@ int main(int argc, char* argv[])
     main__0(argc, reinterpret_cast<intptr_t>(argv), nullptr);
     return 0;
 }
+
+/*
+Potrebujeme ty funkce postupne prevest do citelneho kodu.
+-zkus vzdy vytknout struktury.
+-spravne pojmenovat promenne a funkce, ale nech tam cast oznacujisi adresu, takze to bude jmeno_adresa promenne budou zacinat malymi pismeny funkce velkymi
+-vsechny věci, které resi bios nebo dos, budou smerovat do novych souboru typu port_sound.cpp port_vga.cpp port_mouse.cpp, kod bude inspirovany dosbox-x a remc2, zaklad bude SDL3, terminal budeme emulovat abychom mohli grafiku inicializovat hned na zacatku. Ale bude se to posilat i na standardni vystup, pokud bude.
+-kdyz bude jasne pod jakou skupinu dany kod patri vyclen pro nej specielni soubor, aby se postupne tvořila rozumna struktura kodu
+*/
